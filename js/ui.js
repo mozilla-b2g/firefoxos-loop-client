a48007c2af3a8120e9b90235b3a374c59d105e69
Borja Salguero <borja.salguero@gmail.com>
2014-03-28 21:23:50 +0100

    Bug 990546 - [Loop] [App] Create basic skeleton r=jaoo, ferjm

---
+++ js/ui.js
@@ -0,0 +1,17 @@
+/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+/*
+ * This code is in charge of the UI & User actions in our app.
+ * This is the 'View' part of our MVC.
+ */
+
+(function (exports) {
+
+  var UI = {};
+
+  exports.UI = UI;
+
+}(this));
