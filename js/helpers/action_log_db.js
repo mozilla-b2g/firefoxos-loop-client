e0bc38cf22c5e8108943516751d335b5d37ffe08
Fernando Jiménez <ferjmoreno@gmail.com>
2014-07-29 14:12:23 +0200

    Bug 1045554 - [Loop] Add a way to remove revoked URLs from actions DB. r=borjasalguero

--- js/helpers/action_log_db.js
+++ js/helpers/action_log_db.js
@@ -138,6 +138,7 @@
           urlStore.createIndex('contactId', 'contactId', {
             multiEntry: true
           });
+          urlStore.createIndex('revoked', 'revoked');
         };
       } catch(e) {
         reject(e.message);
@@ -424,25 +425,53 @@
     }, 'readwrite', [aObjectStore]);
   }
 
-  function _deleteRecord(aCallback, aObjectStore, aKey) {
+  /**
+   * Deletes specific record given its keys or all records matching a filter.
+   *
+   * param aFilter.
+   *       So far the only use case that we found is an 'equal' query,
+   *       so we will be taking an object of this kind:
+   *       {
+   *         key: <any>,
+   *         index: {
+   *           name: <string>,
+   *           value: <any>
+   *         }
+   *       }
+   *       where we would expect 'key' or 'index' but not both.
+   */
+  function _deleteRecord(aCallback, aObjectStore, aFilter) {
     _checkCallback(aCallback);
 
-    if (!aKey) {
-      _clearObjectStore(aCallback, aObjectStore);
-      return;
-    }
     _newTxn(function(error, txn, store) {
       if (error) {
         aCallback(error);
         return;
       }
 
-      if (!Array.isArray(aKey)) {
-        aKey = [aKey];
-      }
+      if (aFilter.index) {
+        if (!store.indexNames.contains(aFilter.index.name)) {
+          aCallback('INVALID_FILTER');
+          return;
+        }
+        var range = IDBKeyRange.only(aFilter.index.value);
+        store.index(aFilter.index.name).openCursor(range)
+             .onsuccess = function(event) {
+          var cursor = event.target.result;
+          if (!cursor || !cursor.value) {
+            return;
+          }
+          cursor.delete(cursor.value);
+          cursor.continue();
+        };
+      } else {
+        if (!Array.isArray(aFilter.key)) {
+          aFilter.key = [aFilter.key];
+        }
 
-      for (var i = 0, l = aKey.length; i < l; i++) {
-        store.delete(aKey[i]);
+        for (var i = 0, l = aFilter.key.length; i < l; i++) {
+          store.delete(aFilter.key[i]);
+        }
       }
 
       txn.oncomplete = function() {
@@ -815,11 +844,22 @@
     },
 
     deleteCalls: function(aCallback, aCalls) {
-      _deleteRecord(aCallback, _dbCallStore, aCalls);
+      _deleteRecord(aCallback, _dbCallStore, {
+        key: aCalls
+      });
     },
 
     deleteUrls: function(aCallback, aUrls) {
-      _deleteRecord(aCallback, _dbUrlStore, aUrls);
+      _deleteRecord(aCallback, _dbUrlStore, {
+        key: aUrls
+      });
+    },
+
+    deleteRevokedUrls: function(aCallback) {
+      _deleteRecord(aCallback, _dbUrlStore, {
+        index: 'revoked',
+        value: true
+      });
     },
 
     invalidateContactsCache: function(aCallback) {
