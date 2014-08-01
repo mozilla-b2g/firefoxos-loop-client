f6c5f4732825ceddf2e976ca4850f13539a82236
Fernando Jiménez <ferjmoreno@gmail.com>
2014-08-01 09:33:09 +0200

    Move to production \o/

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ Config = {
   //   Development: 'http://loop.dev.mozaws.net'
   //   Stage: 'https://loop.stage.mozaws.net'
   //   Prod: 'https://loop.services.mozilla.com'
-  server_url: 'https://loop-dev.stage.mozaws.net'
+  server_url: 'https://loop.services.mozilla.com'
 };
