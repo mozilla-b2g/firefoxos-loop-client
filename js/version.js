8f04cc8021d5ce4f435f86eab4d979c5c94b46b8
crdlc <crdlc@tid.es>
2014-10-09 15:36:28 +0200

    Bump up version

--- js/version.js
+++ js/version.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-Version = { id: 'ce63886' };
-=======
-Version = { id: 'dfb68a8' };
->>>>>>> Bug 1080356 - [Loop] You are logged in as string is not localized
+Version = { id: 'b513867' };
