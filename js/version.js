07fe9908be3439192f68f03ebdb750b2a023af82
crdlc <crdlc@tid.es>
2014-10-10 14:40:35 +0200

    Bump up version

--- js/version.js
+++ js/version.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-Version = { id: '4399522' };
-=======
-<<<<<<< HEAD
-Version = { id: '695ba08' };
-=======
-Version = { id: '5827ec1' };
->>>>>>> Bug 1079860 - Use WebApps manifests instead of locales.ini
->>>>>>> Bug 1079860 - Use WebApps manifests instead of locales.ini
+Version = { id: '59294fd' };
